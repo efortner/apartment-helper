93551d32430c8fa4933b079fc5e4166f44856b9a
Ethan Fortner <30123404+efortner@users.noreply.github.com>
2025-08-27 16:47:12 -0700

    feat: highlight changes between existing and new listings (#7)
    Co-authored-by: Ethan Fortner <ethan@anaplian.io>

---
+++ src/adapters/save-apartment-scanner.ts
@@ -0,0 +1,20 @@
+import { Apartment, ApartmentFetch, ApartmentScan } from '../types/apartment';
+import * as fs from 'node:fs';
+
+export interface SaveApartmentScannerProps {
+  readonly directory: string;
+  readonly saveApartmentCache: ApartmentFetch;
+}
+
+export class SaveApartmentScanner implements ApartmentScan {
+  constructor(private readonly props: SaveApartmentScannerProps) {}
+
+  public readonly scan = (): Promise<Apartment[]> =>
+    Promise.all(
+      fs
+        .readdirSync(this.props.directory)
+        .filter((file) => file.endsWith('.json'))
+        .map((fileName) => fileName.split('.')[0]!)
+        .map((apartmentId) => this.props.saveApartmentCache.fetch(apartmentId)),
+    );
+}
